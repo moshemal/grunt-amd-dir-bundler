4a8d1def11b55904903e0fc62cf6242ffa56a6b6
Moshe Malka <moshemal@gmail.com>
2016-04-24 12:45:54 +0300

    adding first test

---
+++ test/expected/main5.js
@@ -0,0 +1,23 @@
+define(["sandbox"], function() {
+  var __modules = {};
+  __modules["utils/1"] = (function() {
+    'use strict';
+    var str = "I am utils/1";
+    return {
+      name: "utils/1",
+      deps: [],
+      privates: [str]
+    };
+  })();
+  __modules["html/1.html"] = (function() {
+    return '<div>bla bla</div>';
+  })();
+  __modules["main5"] = (function(utils, sandbox, html) {
+    'use strict';
+    return {
+      name: "1/main5",
+      deps: [utils, sandbox, html]
+    };
+  })(__modules["utils/1"], arguments[0], __modules["html/1.html"]);
+  return __modules["main5"];
+});
---
+++ test/fixtures/main5.js
@@ -0,0 +1,10 @@
+/**
+ * Created by moshemal on 4/20/16.
+ */
+define(['./utils/1', 'sandbox', 'text!./html/1.html'], function(utils, sandbox, html){
+  'use strict';
+  return {
+    name: "1/main5",
+    deps: [utils, sandbox, html]
+  };
+});
